99e15cfcde58ae51e1d0ab6e74fedf1510ac65f2
Tanya <tanyashrivastava05@gmail.com>
2017-11-28 03:03:48 +0530

    SVD implemented

--- training.py
+++ training.py
@@ -7,7 +7,9 @@ import os
 from re import sub
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem.porter import PorterStemmer
-import pandas as pd
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_similarity
+import scipy
 
 path = './cases_2017'
 token_dict = {}
@@ -41,7 +43,7 @@ for dirpath, dirs, files in os.walk(path):
     #os.walk() generates the file names in a directory tree by walking the tree
     for f in files:
         fname = os.path.join(dirpath, f) #creates filename as path+file
-        print "fname=", fname
+        #print "fname=", fname
         with open(fname) as pearl:
             text = pearl.read()
             #remove unwanted utf-8 characters
@@ -53,7 +55,26 @@ tfidf = TfidfVectorizer(tokenizer=tokenize)#expects list
 #Fit the TfIdf model, and Transform a document into TfIdf coordinates
 tfs = tfidf.fit_transform(token_dict.values())
 
-print tfs
-# feature_names = tfidf.get_feature_names()
-# print feature_names
-print tfidf.vocabulary_
+#print tfs
+feature_names = tfidf.get_feature_names()
+#print feature_names
+#print tfidf.vocabulary_
+print tfs.shape
+
+# svd = TruncatedSVD(n_components = 5)
+# svdMatrix = svd.fit_transform(tfs)
+tfs_matrix = [[0 for x in range(tfs.shape[1])] for y in range(tfs.shape[0])] 
+i = 0
+while i < tfs.shape[0]:
+    j = 0
+    while j < tfs.shape[1]:
+        tfs_matrix[i][j] = tfs[i,j]
+        j=j+1
+    i=i+1
+
+
+u,sigma,vt = scipy.linalg.svd(tfs_matrix)
+
+ #Reconstruct MATRIX'
+reconstructedMatrix= scipy.dot(scipy.dot(u,scipy.linalg.diagsvd(sigma,tfs.shape[0],len(vt))),vt)
+print reconstructedMatrix
